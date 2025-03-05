94dcdff8942153fef7b802c8a80355d5d5b012da
Sydney <scsu@ucsd.edu>
2025-03-04 18:36:47 -0800

    contact

---
+++ script.js
@@ -0,0 +1,19 @@
+/*** Form Handling ***
+  Purpose:
+  - When the user submits the form, the name and state they
+    entered should be added to the list of participants.
+***/
+
+
+// Step 1: Add your query for the submit RSVP button here
+
+
+const addParticipant = (event) => {
+    // Step 2: Write your code to manipulate the DOM here
+
+
+    event.preventDefault();
+}
+
+
+// Step 3: Add a click event listener to the submit RSVP button here
